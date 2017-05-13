1ad16cef62b51f4dea6b4d76d6008685e1d62e16
Emmanuel Lodovice <name3anad@gmail.com>
2017-05-13 10:21:50 +0800

    added in styles

--- tweet/models.py
+++ tweet/models.py
@@ -9,3 +9,6 @@ class Tweet(models.Model):
     owner = models.ForeignKey(User, related_name='tweets')
     content = models.CharField(max_length=255)
     when_created = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return '{} by {}'.format(self.content, self.owner.username)
